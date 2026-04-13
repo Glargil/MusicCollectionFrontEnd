0047c1cb30d04d3e4c74b8572037dda72c78fe59
Egil Bukhave Edström <ege001@edu.zealand.dk>
2026-04-13 11:00:26 +0200

    first commit

---
+++ Index.js
@@ -0,0 +1,25 @@
+const baseURL = "https://musiccollectionrest.azurewebsites.net/api/Record"
+
+Vue.createApp({
+    data() {
+        return {
+            records:[],
+            id:null,
+            title:"",
+            artist:"",
+            publicationYear:null,
+            duration:null
+        }
+    },
+    methods: {
+getAll(){
+    axios.get(baseURL)
+    .then(response => {
+        this.records = response.data
+    })
+    .catch(error => {
+        console.error(error)
+    })
+}
+    }    
+    }).mount("#app")
