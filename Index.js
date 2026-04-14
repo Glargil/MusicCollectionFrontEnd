a6660c58c615e31014b0eed9e4fbf1c7dfd1dfa3
thecorf <mim010@edu.zealand.dk>
2026-04-14 10:48:43 +0200

    login

--- Index.js
+++ Index.js
@@ -1,5 +1,6 @@
 const baseURL = "https://musiccollectionrest.azurewebsites.net/api/Record"
 
+const authUrl = "https://musiccollectionrest.azurewebsites.net/api/Auth/login"
 Vue.createApp({
     data() {
         return {
@@ -8,12 +9,45 @@ Vue.createApp({
             title:"",
             artist:"",
             publicationYear:null,
-            duration:null
+            duration:null,
+            loggedIn:false,
+            auth:{
+                username:"",
+                password:""
+             },
+             authmessage:null,
+             jwtToken:null,
+             role:null
         }
     },
     methods: {
+        login() {
+            axios.post(authUrl, this.auth)
+                .then(response => {
+                    this.jwtToken = response.data.token;
+                    this.role = response.data.role;
+                    this.loggedIn = true;
+                    this.authMessage = "Authentication successful";
+                    this.getAll(); 
+                }).catch(ex => {
+                    this.authMessage = "Authentication failed - " + ex.message;
+                });
+        },
+        logout() {
+            this.jwtToken = null;
+            this.role = null;
+            this.loggedIn = false;
+            this.auth = { username: "", password: "" };
+            this.records = [];
+            this.authMessage = "Logged out successfully";
+        },
 getAll(){
-    axios.get(baseURL)
+    axios.get(baseURL, {
+        headers: {
+            Authorization: `Bearer ${this.jwtToken}`
+        }
+    })
+
     .then(response => {
         this.records = response.data
     })
@@ -22,13 +56,17 @@ getAll(){
     })
 },
 SearchRecord(title,artist){
-    axios.get(`${baseURL}?title=${title}&artist=${artist}`)
+    axios.get(`${baseURL}?title=${title}&artist=${artist}`, {
+        headers: {
+            Authorization: `Bearer ${this.jwtToken}`
+        }
+    })
     .then(response => {
         this.records = response.data
     })
     .catch(error => {
         console.error(error)
      })
-    }   
+    }
 } 
     }).mount("#app")
